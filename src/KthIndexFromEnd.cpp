0305fc04ec3e5abc959fe9a32862cf8669c5b866
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-03-10 19:32:14 +0530

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -1,3 +1,5 @@
+
+#include<stdlib.h>
 /*
 OVERVIEW: Given a string, return the letter at Kth index from the end of the string.
 E.g.: Input: "qwertyui", 3. Output: 't' (index starting from zero).
@@ -12,5 +14,22 @@ NOTES:
 */
 
 char KthIndexFromEnd(char *str, int K) {
+
+
+
+
+	// checking the null conditions
+	if (str == NULL || str == "") return '\0';
+
+	int i = 0;
+	// finding the length of the string
+	for (i = 0; str[i] != '\0'; i++);
+	// checking the out of index condition
+	if (K >= i || K<0) return '\0';
+
+	//returing the requried index character.
+	else return str[i - 1 - K];
+
+
 	return '\0';
 }
--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,59 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
-void str_words_in_rev(char *input, int len){
+void reverse(char *input, int i, int j)
+{
+	char temp; int k = 0;
+
+	while (i<j)
+	{
+		temp = input[i];
+		input[i++] = input[j];
+		input[j--] = temp;
+
+	}
 	
 }
+
+
+
+
+
+
+void str_words_in_rev(char *input, int len){
+
+	int i=0,j=len-1; char temp;
+
+
+	while (i<j)
+	{
+		
+
+		temp = input[i];
+		input[i++] = input[j];
+		input[j--] = temp;
+
+	}
+	
+	
+	int prev = 0;
+	
+	for (i = 1; i < len; i++)
+	{
+		if (input[i] == ' '&&input[i - 1] != ' ') {
+			reverse(input, prev, i - 1);
+		}
+		else if (input[i + 1] == '\0' &&input[i] != ' '){
+			reverse(input, prev, i );
+		}
+			
+		
+
+		if ((input[i] == ' '&&input[i+1] != ' ')) prev = i+1;
+		
+
+	}
+
+	
+}
+
